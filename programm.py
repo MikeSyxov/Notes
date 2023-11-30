7d76ef160bef9a37e4abb436d07a9561433d8395
Mikhail T <Mikola301@mail.ru>
2023-11-30 22:44:41 +0700

    добавлена функция выбора 2, функция вывода заметок

--- programm.py
+++ programm.py
@@ -5,7 +5,7 @@ def read_notes_file(file_name):
     with open(file_name, 'r') as f:
         notes = json.load(f)
     return notes
-def save_notes_json(notes, file_name):
+def save_notes_json(notes, file_name): # функция сохранения
     with open(file_name,'w') as f:
         json.dump(notes,f)
 
@@ -17,6 +17,27 @@ def add_notes(notes):
     new_note = {'id': id, 'title': title,'body': body,'timestamp': timestamp}
     notes.append(new_note)
     return notes
+
+# Функция для фильтрации заметок по дате
+def filter_notes_by_date(notes, date):
+    filtered_notes = []
+    for note in notes:
+        note_date = datetime.datetime.strptime(note['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+    if note_date.date() == date:
+        filtered_notes.append(note)
+    return filtered_notes
+
+# Функция для вывода на экран списка заметок
+def print_notes(notes):
+    if not notes:
+        print('Заметок не найдено')
+    else:
+        for note in notes:
+            print(f'ID: {note["id"]}')
+            print(f'Заголовок: {note["title"]}')
+            print(f'Тело заметки: {note["body"]}')
+            print(f'Дата/время: {note["timestamp"]}')
+            print('---')
 
 def main():
     file_name = 'notes.json'
@@ -32,10 +53,21 @@ def main():
         print('7. Выход')
         choice = input('Ваш выбор: ')
         if choice == '1':
-            print(notes)
+            print_notes(notes)
+        elif choice == '2':
+            date_str = input('Введите дату в формате ГГГГ-ММ-ДД: ')
+            date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+            filtered_notes = filter_notes_by_date(notes, date)
+            print_notes(filtered_notes) 
+        elif choice == '3':
+            pass
         elif choice == '4':
             notes = add_notes(notes)
-            save_notes_json(notes, file_name)    
+            save_notes_json(notes, file_name)
+        elif choice == '5':
+            pass
+        elif choice == '6':
+            pass
         elif choice == '7':
             break
 
